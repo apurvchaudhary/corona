48295825c93e3b43550a417eda32044a74babd30
apurvchaudhary <apurv.sirohi@gmail.com>
2020-04-02 11:54:24 +0530

    made comma separated population

--- corona/corona_app/serializers.py
+++ corona/corona_app/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import  serializers
 from corona_app.models import Country, State, District
+from corona_app.services import convert_comma_separated
 
 
 class DistrictSerializer(serializers.ModelSerializer):
@@ -11,16 +12,21 @@ class DistrictSerializer(serializers.ModelSerializer):
 class StateSerializer(serializers.ModelSerializer):
 
     district = DistrictSerializer(source='get_district', many=True)
+    population = serializers.SerializerMethodField()
 
     class Meta:
         model = State
         fields = ('id', 'name', 'population', 'patients', 'district')
+
+    def get_population(self, obj):
+        return convert_comma_separated(obj.population)
 
 
 class CountrySerializer(serializers.ModelSerializer):
 
     state  = StateSerializer(source='get_state', many=True)
     name = serializers.SerializerMethodField()
+    population = serializers.SerializerMethodField()
 
     class Meta:
         model = Country
@@ -28,3 +34,6 @@ class CountrySerializer(serializers.ModelSerializer):
 
     def get_name(self, obj):
         return obj.name.upper()
+
+    def get_population(self, obj):
+        return convert_comma_separated(obj.population)
