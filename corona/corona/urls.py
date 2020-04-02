61ea852f17f23ed83a28c2f63d784b7354498c87
apurvchaudhary <apurv.sirohi@gmail.com>
2020-04-02 20:40:51 +0530

    added media directory, url & root

--- corona/corona/urls.py
+++ corona/corona/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import re_path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     re_path('admin/', admin.site.urls),
     re_path(r'corona/', include('corona_app.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
