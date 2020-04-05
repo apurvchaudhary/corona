1f50fd050068f37d218b83f6efea964e43e5996e
apurvchaudhary <apurv.sirohi@gmail.com>
2020-04-05 18:11:50 +0530

    url modified

--- corona/corona/urls.py
+++ corona/corona/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import re_path, include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    re_path('admin/', admin.site.urls),
-    re_path(r'', include('corona_app.urls')),
+    path('admin/', admin.site.urls),
+    path('corona/', include('corona_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- corona/corona_app/urls.py
+++ corona/corona_app/urls.py
@@ -1,16 +1,16 @@
-from django.urls import re_path
+from django.urls import path, re_path
 from corona_app.views import CountryView, SafetyView, GraphView, StateView, StateGraphView,\
     UpdateDataView, AboutView, CreateDataView
 
 
 urlpatterns = [
-    re_path(r'home/', CountryView.as_view(), name="home"),
-    re_path(r'safety/', SafetyView.as_view(), name="safety-tips"),
-    re_path(r'stategraph/', StateGraphView.as_view(), name="state-graph"),
-    re_path(r'graph/', GraphView.as_view(), name="graph"),
-    re_path(r'pie/', GraphView.as_view(), name="pie"),
-    re_path(r'state/', StateView.as_view(), name="by-state"),
-    re_path(r'updatedata/', UpdateDataView.as_view(), name="update-data"),
-    re_path(r'createdata/', CreateDataView.as_view(), name="create-data"),
-    re_path(r'about/', AboutView.as_view(), name="about"),
+    re_path(r'^home/$', CountryView.as_view(), name="home"),
+    re_path(r'^safety/$', SafetyView.as_view(), name="safety-tips"),
+    re_path(r'^stategraph/$', StateGraphView.as_view(), name="state-graph"),
+    re_path(r'^graph/$', GraphView.as_view(), name="graph"),
+    re_path(r'^pie/$', GraphView.as_view(), name="pie"),
+    re_path(r'^state/$', StateView.as_view(), name="by-state"),
+    re_path(r'^updatedata/$', UpdateDataView.as_view(), name="update-data"),
+    re_path(r'^createdata/$', CreateDataView.as_view(), name="create-data"),
+    re_path(r'^about/$', AboutView.as_view(), name="about"),
 ]
